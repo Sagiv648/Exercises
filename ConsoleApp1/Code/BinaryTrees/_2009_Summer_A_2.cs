987819b58fc419194a291541cdc16f339e6389f3
Sagiv648 <sagivalia11@gmail.com>
2023-04-25 20:07:42 +0300

    solved new problems

---
+++ ConsoleApp1/Code/BinaryTrees/_2009_Summer_A_2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4.CollectionsLib;
+
+namespace ConsoleApp1.Code.BinaryTrees
+{
+    public class _2009_Summer_A_2 : IClassMethods
+    {
+        BinNode<int> root;
+        BinNode<char> sec;
+        public void GenereateInput()
+        {
+            
+        }
+
+        public bool IsRightLeftTree(BinNode<int> root)
+        {
+            if (root == null)
+                return true;
+            bool flag = true;
+            if(root.HasRight())
+            {
+                flag = root.HasLeft();
+            }
+            
+           
+            return flag && IsRightLeftTree(root.GetLeft()) && IsRightLeftTree(root.GetRight());
+                
+        }
+        public void Work()
+        {
+            //X A I O N Y T D S -> preorder
+            
+            
+            
+            //I N O A X D T S Y -> inorder
+
+            //root = X
+            //root.left = A
+            //root.left.left = I
+            //root.left.left.left = N
+            //root.left.left.left.left = O
+            
+        }
+    }
+}
---
+++ ConsoleApp1/Code/LinkedLists/_2009_Summer_A_1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4.CollectionsLib;
+
+namespace ConsoleApp1.Code.LinkedLists
+{
+    public class _2009_Summer_A_1 : IClassMethods
+    {
+        Node<int> node;
+        public void GenereateInput()
+        {
+            
+            
+            int[] input = new int[] { 2, 5, 3, 12 };
+            node = new Node<int>(2);
+            Node<int> tmp = node;
+            
+            for (int i = 0; i < 1; i++)
+            {
+                int j = 0;
+                if (i == 0)
+                    j = 1;
+                for (; j < input.Length; j++)
+                {
+                    tmp.SetNext(new Node<int>(input[j]));
+                    tmp = tmp.GetNext();
+
+                }
+                    
+            }
+            tmp = node;
+            while(tmp != null)
+            {
+                Console.Write($" {tmp} ");
+                tmp = tmp.GetNext();
+            }
+
+
+            
+        }
+        public bool IsTriangularList(Node<int> node)
+        {
+            Node<int> tmp = node;
+            int c = 0;
+            while(tmp != null)
+            {
+                c++;
+                tmp = tmp.GetNext();
+            }
+            if (c == 0 || c % 3 != 0)
+                return false;
+            tmp = node;
+            Node<int> laterThird = node;
+            c /= 3;
+            while (laterThird != null && c > 0)
+            {
+                laterThird = laterThird.GetNext();
+                c--;
+
+            }
+                
+            for(; tmp != null && laterThird != null; tmp = tmp.GetNext(), laterThird = laterThird.GetNext())
+            {
+                if (tmp.GetValue() != laterThird.GetValue())
+                    return false;
+            }
+            return true;
+        }
+        public void Work()
+        {
+            GenereateInput();
+            Console.WriteLine();
+            Console.WriteLine(IsTriangularList(node));
+        }
+    }
+}
